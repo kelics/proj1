aeb464fa8d532aa1a0f868207c326fdc9358a5fc
kelics <846323899jt>
2019-02-19 13:26:29 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<DOCTYPE hmtl>
+    <html>
+    <head>
+    </head>
+    <body>
+    <p>
+
+
+        <?php
+
+        $myArray = provisionedActivity("specs", "mugs", "sausage rolls");
+
+        foreach($provisionedActivity as $x){
+            print "<p>$x</p>";
+        }
+            echo "hi";
+        ?>
+        <h1> zdape</h1>
+    </p>
+    </body>
+
+    </html>
